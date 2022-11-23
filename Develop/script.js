409984132fc9bda9adc6c955c0e2f38635af7a52
RoseCass <rosecassidy1303@gmail.com>
2022-11-23 15:40:59 -0800

    added HTML, got time classes working

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,54 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+// Display today's day and date
+var todayDate = dayjs().format('dddd, MMM D YYYY');
+
+
+$("#currentDay").html(todayDate);
+    saveBtn = document.getElementById("saveBtn") 
+    saveBtn
+
   // TODO: Add a listener for click events on the save button. This code should
+
+
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
+
+  function timeTracker() {
+    //get current number of hours.
+    var hourNow = dayjs().hour();
+    console.log(hourNow)
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").replace("hour-",""));
+        console.log(blockTime, ($(this).attr("id").split("hour-")))
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < hourNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === hourNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+  
+  
+}
+
+timeTracker();
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
