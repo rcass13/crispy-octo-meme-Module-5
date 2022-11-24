deff717b0dc4a375046eb7fb61ce7a95196d2acd
RoseCass <rosecassidy1303@gmail.com>
2022-11-23 17:28:48 -0800

    get items from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -66,23 +66,18 @@ $(function () {
       }
     })
 
-
+    // $("#hour-8")(".description").val(localStorage.getItem);
+    $("#hour-8 > .description").val(localStorage.getItem("8"));
+    $("#hour-9 > .description").val(localStorage.getItem("9"));
+    $("#hour-10 > .description").val(localStorage.getItem("10"));
+    $("#hour-11 > .description").val(localStorage.getItem("11"));
+    $("#hour-12 > .description").val(localStorage.getItem("12"));
+    $("#hour-13 > .description").val(localStorage.getItem("13"));
+    $("#hour-14 > .description").val(localStorage.getItem("14"));
+    $("#hour-15 > .description").val(localStorage.getItem("15"));
+    $("#hour-16 > .description").val(localStorage.getItem("16"));
+    $("#hour-17 > .description").val(localStorage.getItem("17"));
   }
 
   timeTracker();
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
